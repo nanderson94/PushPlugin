93174510af2fc123874f5255326b22849b6f3768
Nicholas Anderson <nander13@gmu.edu>
2014-07-29 18:02:01 -0400

    Restore default example script
    I never intended to push this, whoops. Don't even try, already revoked
    the API key, and unregistered the registration id.

--- Example/server/pushGCM.rb
+++ Example/server/pushGCM.rb
@@ -2,8 +2,8 @@ require 'rubygems'
 require 'pushmeup'
 GCM.host = 'https://android.googleapis.com/gcm/send'
 GCM.format = :json
-GCM.key = "AIzaSyAW1Vq1tpsukwQArwFC6CwTRDkDVS5lfPc"
-destination = ["APA91bGovaAOr-QQhAesfUbWR0bqSR7llmfog_P4kYUl1GWHWE5VonL7xPOGXTxfTy84nyVAdtDJIdUTwlUJpQcucUsF4sfEickLtILXTQU_Uu12f8N0LQArPWGMVQg9_t-L6awBtk3MuwVtZbr8ZtL_OFATbI7fiQ"]
-data = { :type => "ABOUT_HURDLR", :title => "View Hurdlr's info!", :message => "<>\"\\' &)(@# {aaa  aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", :redirectUrl => "pendingExpenses", :redirectData => {},  }
+GCM.key = "API_KEY_GOES_HERE"
+destination = ["REGISTRATION_ID_GOES_HERE"]
+data = {:message => "PhoneGap Build rocks!", :msgcnt => "1", :soundname => "beep.wav"}
 
 GCM.send_notification( destination, data)
